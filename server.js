97683f3edd84bd3bc94a8fbcec8e271047620744
harshwardhan <barkuleharsh205@gmail.com>
2026-06-20 09:45:24 +0530

    craeted the index,cart pages

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, 'dashboard.html'))
 })
 
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/shop', (req, res) => {
+  res.sendFile(path.join(__dirname, 'shop.html'))
+})
+
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname, 'cart.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'))
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
